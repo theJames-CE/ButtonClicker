9c3d9fe21e3bce0d8343aac21679aa1d0b48d855
James Williams <124546382+theJames-CE@users.noreply.github.com>
2023-04-06 18:51:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function message() {
+    alert("Ninja was liked!");
+}
+
+function login(element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(element) {
+    element.remove();
+}
